edf577d836bdeb02afc1d22f0a5cb5808eed3df3
Alexander <hello@alexander.works>
2020-11-15 20:17:33 +0200

    Initial desktop commit

--- AR_RealComp_Test1/ViewController.swift
+++ AR_RealComp_Test1/ViewController.swift
@@ -6,20 +6,55 @@
 //  Copyright © 2020 Alexander Nuzhnyi. All rights reserved.
 //
 
+
 import UIKit
+import ARKit
 import RealityKit
 
-class ViewController: UIViewController {
+class ARController: UIViewController {
+    var arView = ARView(frame: .zero)
     
-    @IBOutlet var arView: ARView!
-    
+    override func loadView() {
+        super.loadView()
+        view.addSubview(arView)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        arView.frame = view.frame
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        Hexagon.loadSceneAsync {(result) in
+            do {
+                let hexagonScene = try result.get()
+                self.arView.scene.addAnchor(hexagonScene)
+
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        arView.session.run(configuration)
     }
 }
+
+//import UIKit
+//import RealityKit
+//
+//class ViewController: UIViewController {
+//
+//    @IBOutlet var arView: ARView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+//    }
+//}
